6874847b8ee051d7cdc90e1e38b900791dc7f33c
Tonto <823498927@qq.com>
2018-05-15 11:00:12 +0800

    爬取数据编码问题修改

--- src/main/java/com/paladin/health/data/parser/PageParser.java
+++ src/main/java/com/paladin/health/data/parser/PageParser.java
@@ -1,8 +1,9 @@
 package com.paladin.health.data.parser;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.util.HtmlUtils;
@@ -11,18 +12,33 @@ import com.paladin.framework.utils.StringUtil;
 
 public class PageParser {
 
-	private String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+	// private String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64)
+	// AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+
+	protected Document getDocument(String url, String charset) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection) ((new URL(url)).openConnection());
+		// 默认就是Get，可以采用post，大小写都行，因为源码里都toUpperCase了。
+		connection.setRequestMethod("GET");
+		// 是否允许缓存，默认true。
+		connection.setUseCaches(Boolean.FALSE);
+		// 设置请求头信息
+		connection.addRequestProperty("Connection", "close");
+		// 设置连接主机超时（单位：毫秒）
+		connection.setConnectTimeout(60000);
+		// 设置从主机读取数据超时（单位：毫秒）
+		connection.setReadTimeout(60000);
+		// 开始请求
+		return Jsoup.parse(connection.getInputStream(), charset, url);
+	}
 
 	protected Document getDocument(String url) throws IOException {
-		Connection conn = Jsoup.connect(url);
-		conn.timeout(60000);
-		// 修改http包中的header,伪装成浏览器进行抓取
-		conn.header("User-Agent", userAgent);
-		return conn.get();
+		String charset = url.contains("http://jbk.39.net") ? "GBK" : "UTF8";
+		return getDocument(url, charset);
 	}
 
 	/**
 	 * 修复html文本
+	 * 
 	 * @param text
 	 * @return
 	 */
