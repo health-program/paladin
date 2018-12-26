e8cb08b180cd63ee46cf30e656ef1c02bfcb7f75
jisanjie <876523329@qq.com>
2018-12-26 16:47:45 +0800

    视频管理首页分页查询
    分页查询

--- src/main/java/com/paladin/health/service/videomanage/VideoService.java
+++ src/main/java/com/paladin/health/service/videomanage/VideoService.java
@@ -10,6 +10,9 @@ import com.paladin.health.mapper.videomanage.VideoMapper;
 import com.paladin.health.model.videomanage.Video;
 import com.paladin.health.service.videomanage.dto.VideoQueryDTO;
 import com.paladin.health.service.videomanage.vo.VideoVO;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.paladin.framework.common.PageResult;
 import com.paladin.framework.core.ServiceSupport;
 
 @Service
@@ -18,8 +21,10 @@ public class VideoService extends ServiceSupport<Video> {
       @Autowired
       private VideoMapper videoMapper;
 
-      public List<VideoVO> searchPageList(VideoQueryDTO query) {
-            return videoMapper.searchPageList(query);
+      public PageResult<VideoVO> searchPageList(VideoQueryDTO query) {
+            Page<VideoVO> page = PageHelper.offsetPage(query.getOffset(), query.getLimit());
+            videoMapper.searchPageList(query);
+            return new PageResult<>(page);
       }
 
       public List<Video> findLabelList() {
