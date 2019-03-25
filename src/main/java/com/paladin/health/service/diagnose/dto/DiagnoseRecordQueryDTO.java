c7b52cef1eedcd1fdb44dfdc416272abb4c38d8e
huangguochen <896599830@qq.com>
2019-03-25 16:22:38 +0800

    病人历史诊断记录查询接口参数修改

--- src/main/java/com/paladin/health/service/diagnose/dto/DiagnoseRecordQueryDTO.java
+++ src/main/java/com/paladin/health/service/diagnose/dto/DiagnoseRecordQueryDTO.java
@@ -2,8 +2,6 @@ package com.paladin.health.service.diagnose.dto;
 
 import com.paladin.framework.common.OffsetPage;
 
-import java.util.Date;
-
 /**
  * <>
  *
@@ -12,23 +10,53 @@ import java.util.Date;
  */
 public class DiagnoseRecordQueryDTO  extends OffsetPage {
 
-    private Date startTime;
+    private String name;
 
-    private Date endTime;
+    private String targetId;
 
-    public Date getStartTime() {
-        return startTime;
+    private String factorCode;
+
+    private Integer factorType;
+
+    private Integer factorLevel;
+
+    public String getName() {
+        return name;
     }
 
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Date getEndTime() {
-        return endTime;
+    public String getTargetId() {
+        return targetId;
     }
 
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
+    public void setTargetId(String targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getFactorCode() {
+        return factorCode;
+    }
+
+    public void setFactorCode(String factorCode) {
+        this.factorCode = factorCode;
+    }
+
+    public Integer getFactorType() {
+        return factorType;
+    }
+
+    public void setFactorType(Integer factorType) {
+        this.factorType = factorType;
+    }
+
+    public Integer getFactorLevel() {
+        return factorLevel;
+    }
+
+    public void setFactorLevel(Integer factorLevel) {
+        this.factorLevel = factorLevel;
     }
 }
