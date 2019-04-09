5408ab84dcd5769b938e3ddf7a3066981476e307
huangguochen <896599830@qq.com>
2019-04-09 10:44:05 +0800

    样式调整

--- src/main/java/com/paladin/health/service/core/xk/XKHealthPrescriptionService.java
+++ src/main/java/com/paladin/health/service/core/xk/XKHealthPrescriptionService.java
@@ -1,7 +1,6 @@
 package com.paladin.health.service.core.xk;
 
 import com.paladin.common.core.ConstantsContainer;
-import com.paladin.common.core.ConstantsContainer.KeyValue;
 import com.paladin.framework.common.BaseModel;
 import com.paladin.framework.utils.JsonUtil;
 import com.paladin.framework.utils.uuid.UUIDUtil;
@@ -47,12 +46,200 @@ public class XKHealthPrescriptionService {
 	/** 熙康评估类型 */
 	public static String CONSTANT_EVALUATE_TYPE = "xk-evaluate-type";
 
+
+
+	/**
+	 * 功能描述: <br>
+	 * 〈体检百科知识获取〉
+	 * @param codes
+	 * @return  java.util.List<com.paladin.health.service.core.xk.response.XKDiseaseKnowledge>
+	 * @author  Huangguochen
+	 * @date  2019/4/8
+	 */
+	@Transactional
+	public List<XKDiseaseKnowledge> diagnoseDiseases(List<String> codes) {
+		List<XKDiseaseKnowledge> diseaseKnowledge = null;
+		if (codes != null && codes.size() > 0) {
+			Set<String> diseaseSet = new HashSet<>(codes);
+			diseaseKnowledge = new ArrayList<>();
+			for (String disease : diseaseSet) {
+				XKDiseaseKnowledge k = getKnowledge(disease);
+				if(k != null) {
+					diseaseKnowledge.add(k);
+				}
+			}
+		}
+		return  diseaseKnowledge;
+	}
+
+	/**
+	 * 功能描述: <br>
+	 * 〈风险评估知识获取〉
+	 * @param condition
+	 * @return  java.util.List<com.paladin.health.service.core.xk.response.XKEvaluation>
+	 * @author  Huangguochen
+	 * @date  2019/4/8
+	 */
+	@Transactional
+	public List<XKEvaluation> diagnoseEvaluation(XKPeopleCondition condition) {
+		List<XKEvaluation> evaluationResultList = null;
+		XKEvaluateCondition evaluateCondition = condition.getCondition();
+		if (evaluateCondition != null) {
+			Map evaluationResult = getEvaluation(evaluateCondition);
+			if (evaluationResult != null) {
+				evaluationResultList = new ArrayList<>(10);
+				for (ConstantsContainer.KeyValue kv : ConstantsContainer.getType(CONSTANT_EVALUATE_TYPE)) {
+
+					String code = kv.getKey();
+					String name = kv.getValue();
+
+					Map single = (Map) evaluationResult.get(code);
+					if (single == null)
+						continue;
+					String status = (String) single.get("status");
+
+					// 如果不是状态10000则评估出错，可能是参数不全
+					if (!"0000".equals(status)) {
+						continue;
+					}
+
+					String riskResultStr = (String) single.get("result");
+					if (riskResultStr == null || riskResultStr.length() == 0) {
+						continue;
+					}
+
+					Map riskResult = null;
+					try {
+						riskResult = JsonUtil.parseJson(riskResultStr, Map.class);
+					} catch (IOException e) {
+						logger.error("评估[" + name + "]时非法解析json字符串格式：" + riskResultStr);
+						continue;
+					}
+
+					if (riskResult == null) {
+						continue;
+					}
+
+					String riskLevelName = (String) riskResult.get("riskLevel");
+					int riskLevel = 0;
+
+					// 熙康返回的是中文危险等级名称，这里对其归纳总结给出等级
+					if ("极低风险".equals(riskLevelName)) {
+						riskLevel = XKEvaluation.LEVEL_VERY_LOW;
+					} else if ("低风险".equals(riskLevelName)) {
+						riskLevel = XKEvaluation.LEVEL_LOW;
+					} else if ("中风险".equals(riskLevelName) || "中等风险".equals(riskLevelName)) {
+						riskLevel = XKEvaluation.LEVEL_MIDDLE;
+					} else if ("高风险".equals(riskLevelName)) {
+						riskLevel = XKEvaluation.LEVEL_HIGH;
+					} else if ("极高风险".equals(riskLevelName)) {
+						riskLevel = XKEvaluation.LEVEL_VERY_HIGH;
+					} else {
+						logger.error("评估[" + name + "]时出现未知风险等级：" + riskLevelName);
+						continue;
+					}
+
+					String suggest = (String) riskResult.get("suggest");
+					evaluationResultList.add(new XKEvaluation(code, name, riskLevel, riskLevelName, suggest));
+				}
+
+				// 按危险等级排序
+				if (evaluationResultList.size() > 0) {
+					evaluationResultList.sort(new Comparator<XKEvaluation>() {
+						@Override
+						public int compare(XKEvaluation o1, XKEvaluation o2) {
+							return o2.getRiskLevel() - o1.getRiskLevel();
+						}
+					});
+
+				}
+			}
+		}
+		String identificationId = condition.getIdentificationId();
+		// 保存数据，没有身份证则不进行数据持久化
+		if (identificationId != null && identificationId.length() > 0) {
+			String name = condition.getName();
+			String cellphone = condition.getCellphone();
+			Date birthday = condition.getBirthday();
+			Date now = new Date();
+
+			// 性别可以从评估那去获取
+			Integer sex = condition.getSex();
+			if (sex == null) {
+				if (evaluateCondition != null) {
+					String sexStr = evaluateCondition.getSex();
+					if (sexStr != null && sexStr.length() > 0) {
+						try {
+							sex = Integer.valueOf(sexStr);
+						} catch (Exception e) {
+
+						}
+					}
+				}
+			}
+
+			// 保存或更新目标病人的信息（如果是空则不会更新掉原先数据）
+			DiagnoseTarget target = diagnoseTargetService.get(identificationId);
+
+			if (target == null) {
+				target = new DiagnoseTarget();
+				target.setId(identificationId);
+				target.setName(name);
+				target.setBirthday(birthday);
+				target.setCellphone(cellphone);
+				target.setSex(sex);
+				target.setCreateTime(now);
+				target.setUpdateTime(now);
+				diagnoseTargetService.save(target);
+			} else {
+				target.setId(identificationId);
+
+				if (name != null && name.length() > 0)
+					target.setName(name);
+				if (birthday != null)
+					target.setBirthday(birthday);
+				if (cellphone != null && cellphone.length() > 0)
+					target.setCellphone(cellphone);
+				if (sex != null)
+					target.setSex(sex);
+				target.setUpdateTime(now);
+				diagnoseTargetService.update(target);
+			}
+			// 替换目标病人存在的危险因素（疾病，风险）
+			List<DiagnoseTargetFactor> targetFactors = new ArrayList<>();
+
+			if (evaluationResultList != null && evaluationResultList.size() > 0) {
+				for (XKEvaluation evaluation : evaluationResultList) {
+					targetFactors.add(new DiagnoseTargetFactor(identificationId, evaluation.getCode(), DiagnoseTargetFactor.FACTOR_TYPE_RISK,
+							evaluation.getRiskLevel(), now));
+				}
+			}
+
+			diagnoseRecordFactorService.removeByTarget(identificationId);
+			if (targetFactors.size() > 0) {
+				diagnoseRecordFactorService.batchSave(targetFactors);
+			}
+
+			// 保存目标病人本次请求和返回数据
+			DiagnoseRecord record = new DiagnoseRecord();
+			String diagnoseId = UUIDUtil.createUUID();
+			record.setId(diagnoseId);
+			record.setTargetId(identificationId);
+			record.setTargetCondition(JsonUtil.getJson(condition));
+			record.setPrescription(JsonUtil.getJson(evaluationResultList));
+			record.setType(DiagnoseRecord.TYPE_XK);
+			record.setCreateTime(now);
+
+			diagnoseRecordService.save(record);
+		}
+		return  evaluationResultList;
+	}
 	/**
 	 * 健康处方获取服务方法
 	 * 
 	 * @param condition
 	 * @return
-	 */
+
 	@Transactional
 	public XKHealthPrescription diagnose(XKPeopleCondition condition) {
 		XKHealthPrescription result = new XKHealthPrescription();
