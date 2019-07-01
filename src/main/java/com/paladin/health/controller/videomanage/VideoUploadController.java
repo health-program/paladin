9ce49ce268760875e8891a0876fb46b5e0895258
Tonto <823498927@qq.com>
2019-07-01 14:28:19 +0800

    视频上传

---
+++ src/main/java/com/paladin/health/controller/videomanage/VideoUploadController.java
@@ -0,0 +1,27 @@
+package com.paladin.health.controller.videomanage;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.paladin.framework.core.exception.BusinessException;
+import com.paladin.health.core.HealthUserSession;
+
+@Controller
+@RequestMapping("/health/video/upload")
+public class VideoUploadController {
+
+	@Value("${paladin.uploader.access-key}")
+	private String accessKey;
+	@Value("${paladin.uploader.url}")
+	private String uploaderUrl;
+
+	@RequestMapping("/index")
+	public String index() {
+		if (!HealthUserSession.getCurrentUserSession().isSystemAdmin()) {
+			throw new BusinessException("没有权限访问");
+		}
+		return "redirect:" + uploaderUrl + "?accessKey=" + accessKey;
+	}
+
+}
